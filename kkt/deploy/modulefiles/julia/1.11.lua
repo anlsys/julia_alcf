f774380b9b3f9480fc8629bd7eefca2df7cadca4
Michel Schanen <michel.schanen@gmail.com>
2025-12-02 15:00:43 -0600

    Fix KKT

--- kkt/deploy/modulefiles/julia/1.11.lua
+++ kkt/deploy/modulefiles/julia/1.11.lua
@@ -2,22 +2,23 @@
 Module julia
 ]]--
 
+local system_path = "SYSTEM_DEPOT_PATH"
+local version = "1.11"
 help([[Julia programming language]])
 
-whatis("Julia programming language version 1.11")
+whatis("Julia programming language version " .. version)
+
 
 -- Set JULIA_DEPOT_PATH to default if not already set
 local julia_depot = os.getenv("JULIA_DEPOT_PATH")
 if not julia_depot then
     local home = os.getenv("HOME")
     julia_depot = pathJoin(home, ".julia")
-    setenv("JULIA_DEPOT_PATH", julia_depot)
     LmodMessage("JULIA_DEPOT_PATH not set. Using default: " .. julia_depot .. "\n" ..
                 "To use a different location, set JULIA_DEPOT_PATH before loading this module.")
 end
 
-local version = "1.11"
-local prefix = "SYSTEM_DEPOT_PATH/julia_binaries"
+local prefix = pathJoin(system_path, "julia_binaries")
 local julia_dir = pathJoin(prefix, "julia-" .. version)
 
 -- Check if Julia installation directory exists
@@ -27,13 +28,21 @@ if not isDir(julia_dir) then
               "Please run the setup script to install Julia " .. version .. ".")
 end
 
-unload("xalt")
+load("cuda")
+load("libcudss")
 family("julia")
 conflict("julia")
-conflict("xalt")
 
 prepend_path("PATH", pathJoin(julia_dir, "bin"))
 prepend_path("MANPATH", pathJoin(julia_dir, "share/man"))
 prepend_path("LIBRARY_PATH", pathJoin(julia_dir, "lib"))
 prepend_path("LD_LIBRARY_PATH", pathJoin(julia_dir, "lib"))
-setenv("JULIA_DEPOT_PATH", "SYSTEM_DEPOT_PATH")
+local sys_prefs = pathJoin(system_path, "environments/v" .. version)
+if not isDir(sys_prefs) then
+    LmodError("Julia " .. version .. " has not been set up.\n" ..
+              "Expected installation directory not found: " .. sys_prefs .. "\n" ..
+              "Please run the setup script to install Julia " .. version .. ".")
+end
+setenv("JULIA_LOAD_PATH", "@:@v#.#:@stdlib:" .. sys_prefs)
+local tmpdir = pathJoin(julia_depot, "tmp")
+
