fb7fddcaff97c2b929f1b71d5f3e91f16309f14b
Michel Schanen <mschanen@anl.gov>
2025-12-03 18:23:28 +0000

    Aurora update

--- aurora/modulefiles/julia/1.10.lua
+++ aurora/modulefiles/julia/1.10.lua
@@ -2,9 +2,12 @@
 Module julia
 ]]--
 
+local system_path = "SYSTEM_DEPOT_PATH"
+local version = "1.12"
 help([[Julia programming language]])
 
-whatis("Julia programming language version 1.10")
+whatis("Julia programming language version " .. version)
+
 
 -- Set JULIA_DEPOT_PATH to default if not already set
 local julia_depot = os.getenv("JULIA_DEPOT_PATH")
@@ -16,8 +19,7 @@ if not julia_depot then
                 "To use a different location, set JULIA_DEPOT_PATH before loading this module.")
 end
 
-local version = "1.10"
-local prefix = "SYSTEM_DEPOT_PATH/julia_binaries"
+local prefix = pathJoin(system_path, "julia_binaries")
 local julia_dir = pathJoin(prefix, "julia-" .. version)
 
 -- Check if Julia installation directory exists
@@ -36,7 +38,19 @@ prepend_path("PATH", pathJoin(julia_dir, "bin"))
 prepend_path("MANPATH", pathJoin(julia_dir, "share/man"))
 prepend_path("LIBRARY_PATH", pathJoin(julia_dir, "lib"))
 prepend_path("LD_LIBRARY_PATH", pathJoin(julia_dir, "lib"))
-append_path("JULIA_DEPOT_PATH", "SYSTEM_DEPOT_PATH")
+local sys_prefs = pathJoin(system_path, "environments/v" .. version)
+if not isDir(sys_prefs) then
+    LmodError("Julia " .. version .. " has not been set up.\n" ..
+              "Expected installation directory not found: " .. sys_prefs .. "\n" ..
+              "Please run the setup script to install Julia " .. version .. ".")
+end
+setenv("JULIA_LOAD_PATH", "@:@v#.#:@stdlib:" .. sys_prefs)
+local tmpdir = pathJoin(julia_depot, "tmp")
+setenv("TMPDIR", tmpdir)
+-- Check if directory exists; if not, create it
+if (not isDir(tmpdir)) then
+    execute{cmd="mkdir -p " .. tmpdir, modeA={"load"}}
+end
 
 setenv("HTTP_PROXY", "http://proxy.alcf.anl.gov:3128")
 setenv("HTTPS_PROXY", "http://proxy.alcf.anl.gov:3128")
